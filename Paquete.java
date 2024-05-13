66dd482cbe3664327c5bf1d90eb358e010816d71
DidierSaho <95487810+DidierSaho@users.noreply.github.com>
2024-05-13 18:18:54 -0300

    Update Paquete.java

--- Paquete.java
+++ Paquete.java
@@ -7,10 +7,9 @@ public class Paquete {
     //si puede repetir uso un list
 
     public void setItinerario(LugarTuristico unLugar){
-        int indice = this.listaLugares.size();
         if (this.listaLugares.size() == 0) {
             listaLugares.add(unLugar);
-        } else if (!this.listaLugares.get(indice-1).getNombre().equals(unLugar.getNombre())) {
+        } else if (!this.listaLugares.getLast().getNombre().equals(unLugar.getNombre())) {
             listaLugares.add(unLugar);
         }
     //get(indice-1).getNombre().equals
